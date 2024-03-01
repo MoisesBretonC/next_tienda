d82d419e891e5b5b550e42a66d5c199fcb8a0559
Moises <moisesbreton11@gmail.com>
2024-02-29 19:59:41 -0600

    use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 
+
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  console.log("hola esquites");
   return (
+
     <main className={styles.main}>
      
      <h1>hola mundo cruel</h1>
