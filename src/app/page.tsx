281131c2580fe7280096a6b16755bcd9b9ada0a6
Moises <moisesbreton11@gmail.com>
2024-02-29 18:19:01 -0600

    capitulo 4

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+     
+     <h1>hola mundo cruel</h1>
+
+    </main>
+  );
+}
