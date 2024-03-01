d64a10b24a3addaa68b0098bf60050adec54b743
Moises <moisesbreton11@gmail.com>
2024-02-29 18:35:30 -0600

    rutas dinamicas

---
+++ src/app/store/[category]/page.tsx
@@ -0,0 +1,18 @@
+interface CategoryProps{
+    params: {
+        category: string[],
+        searchParams: string
+    }
+}
+
+export default function Category(props: CategoryProps){
+    console.log(props) 
+    const { category } = props.params
+
+    console.log(category);
+
+    return(
+
+        <h1>Categoria: {category}</h1>
+    )
+}
