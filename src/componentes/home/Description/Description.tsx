115eb8e6a37bbb32035b54997196dc0b49dcb7a7
Moises <moisesbreton11@gmail.com>
2024-03-04 22:13:22 -0600

    optimizacion de imagenes

--- src/componentes/home/Description/Description.tsx
+++ src/componentes/home/Description/Description.tsx
@@ -1,7 +1,17 @@
+import styles from './Description.module.sass';
+import Image from 'next/image';
+
 export const Description = () => {
     return (
         <section>
-            <img src="/images/terreneitor.jpg" alt='Products marketplace'/>
+            <img
+             src="/images/terreneitor.jpg" 
+             alt='Products marketplace'
+             width={500}
+             height={300}
+            //  priority={false}
+            // quality={100}
+             />
             <h2>Description</h2>
 
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt possimus iusto harum, quam maiores sapiente? Harum vero assumenda quae! Iste ducimus quae tempore cupiditate itaque autem corporis laboriosam officia maiores.</p>
