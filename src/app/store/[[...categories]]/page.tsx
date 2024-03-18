4417f08d36a084d3a74a835fc1ba8f45dc3d6c7e
Moises <moisesbreton11@gmail.com>
2024-03-17 21:07:46 -0600

    4.1 implementando not found y errores globales

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -6,10 +6,8 @@ interface CategoryProps{
 }
 
 export default function Category(props: CategoryProps){
-    console.log(props)
     const { categories } = props.params
 
-    console.log(categories);
 
     return(
 
