3d98601d5a7f8cf5cbfc9ac44fb94dfd3908382c
Moises <moisesbreton11@gmail.com>
2024-04-15 23:52:40 -0600

    4.6 implementa pagina de tienda

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,16 +1,29 @@
-interface CategoryProps{
+
+import { ProductsWrapper } from "app/componentes/home/Store/ProductsWrapper"
+// import { getCollections, getCollectionsProducts } from "app/services/shopify/collections"
+import { getProducts } from "app/services/shopify" 
+
+interface CategoriesProps {
     params: {
-        categories: string[],
+        categories: string[]
         searchParams: string
     }
 }
 
-export default function Category(props: CategoryProps){
+export default async function Categories(props: CategoriesProps){
+    
+
     const { categories } = props.params
-
-
+    let products = []
+    const collections = await getCollections()
+    if(categories?.length > 0) {
+        const selectedCollectionId = collections.find((collection) => collection.handle === categories[0]).id
+        products = await getCollectionsProducts(selectedCollectionId)
+    }else{
+        products = await getProducts()
+    }
+    console.log(products)
     return(
-
-        <h1>Categoria: {categories}</h1>
-    )
-}
+        <ProductsWrapper products={products}/>
+    );
+    }
