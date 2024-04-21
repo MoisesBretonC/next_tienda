b22852c922fd0b64134fff8f7d3f4f29d714779e
Moises <moisesbreton11@gmail.com>
2024-04-20 23:17:28 -0600

    5.3 proceso de signup con graphql

--- src/componentes/signup/NewAccountForm/NewAccountForm.tsx
+++ src/componentes/signup/NewAccountForm/NewAccountForm.tsx
@@ -8,10 +8,10 @@ export const NewAccountForm = () => {
   const [errors, setErrors] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
-    handleCreateUser(formData)
+    await handleCreateUser(formData)
   }
 
   return (
@@ -20,6 +20,7 @@ export const NewAccountForm = () => {
       <form className={styles.NewAccountForm__form} onSubmit={handleSubmit}>
         <input type="text" name="first_name" placeholder="Name" disabled={loading} />
         <input type="text" name="last_name" placeholder="Lastname" disabled={loading} />
+        
         <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" disabled={loading} />
         <input type="text" name="phone" placeholder="phone number" pattern="^[0-9]*$" disabled={loading} />
         <input type="password" name="password" placeholder="password" disabled={loading} />
