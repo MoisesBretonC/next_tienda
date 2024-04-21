b02cebb4f2fd96cc00b17825283b201055f40486
Moises <moisesbreton11@gmail.com>
2024-04-20 20:14:14 -0600

    4.12 validando cache con revalidate tag y cache

--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -3,7 +3,7 @@ import { env } from 'app/config/env'
 export const shopifyUrls = {
     products: {
       'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
-      mainProducts: `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/collections/425365307612/products.json`,
+      mainProducts: `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/collections/472267424037/products.json`,
     },
     collections: {
         'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`,
