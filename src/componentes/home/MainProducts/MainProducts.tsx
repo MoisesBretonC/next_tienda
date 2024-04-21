d31fb512672409f8be260aa53704c302c46ac13f
Moises <moisesbreton11@gmail.com>
2024-04-20 19:54:55 -0600

    4.11 Como funciona el fetch y el cache

--- src/componentes/home/MainProducts/MainProducts.tsx
+++ src/componentes/home/MainProducts/MainProducts.tsx
@@ -1,17 +1,16 @@
+import { getMainProducts } from 'app/services/shopify/products'
 import Image from 'next/image'
 import styles from './MainProducts.module.sass'
-import { getProducts } from 'app/services/shopify/products'
 
 export const MainProducts = async () => {
-   const response = await fetch('https://localhost:3000/api')
+    const products = await getMainProducts()
 
-   const {products} = await response.json()
 
     return(
         <section className={styles.MainProducts}>
         <h3>✨ New products released!</h3>
         <div className={styles.MainProducts__grid}>
-        {products?.map((product:any) => {
+            {products?.map((product:any) => {
                 const imageSrc = product.images[0].src;
                 return (
                     <article key={product.id}>
